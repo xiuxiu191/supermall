f2db411861801aaf3c8add2494ecd7f788737d8a
xiuxiu <yjy191@126.com>
2020-09-17 20:58:00 +0800

    'vue.config.js文件把多余的代码去掉了'

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   configureWebpack: {
     /*
-    	启动默认打开浏览器  open: true
+    启动默认打开浏览器  open: true
     */
     devServer: {
-      open: true,
+      open: true
     },
     /*
       配置路径别名
     */
     resolve: {
       alias: {
-        'assets': '@/assets',
-        'common': '@/common',
-        'components': '@/components',
-        'network': '@/network',
-        'views': '@/views',
+        assets: '@/assets',
+        common: '@/common',
+        components: '@/components',
+        network: '@/network',
+        views: '@/views'
       }
     }
   }
