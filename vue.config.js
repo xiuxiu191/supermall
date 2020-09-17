f10ec78ad9a3f386a8f0ffb54c336d9501972a56
xiuxiu <yjy191@126.com>
2020-09-17 16:57:57 +0800

    '更改了一些配置'

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,22 @@
-/*
-  配置路径别名
-*/
 module.exports = {
-  devServer:{
-	  post: 8080,
-	  host: '127.0.0.1'
-  }
-  configureWebpack:{
+  configureWebpack: {
+    /*
+    	启动默认打开浏览器  open: true
+    */
+    devServer: {
+      open: true,
+    },
+    /*
+      配置路径别名
+    */
     resolve: {
-          alias: {
-            'assets': '@/assets',
-            'common': '@/common',
-            'components': '@/components',
-            'network': '@/network',
-            'views': '@/views',
-          }
-        }
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
   }
 }
